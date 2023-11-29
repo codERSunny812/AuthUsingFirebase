fee2f0f9b917f2c2730d4c4959a2e6a17314661c
codersunny812 <sengersunny448@gmail.com>
2023-11-29 15:59:56 +0530

    main redering file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Status from './Component/status';
+
+function App() {
+  return (
+    <>
+    <div className="mainCont">
+        <h1>Authentication in react using firebase</h1>
+        <Status />
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
